ba24e3ea79dd97117d42aca25a7d113dfb197ffa
Alexjandrohum <alejandrohermen.97@gmail.com>
2022-02-10 12:41:48 -0600

    Se agrega estructura basica del menu principal

---
+++ app/src/main/java/com/hermen/learnenglish/adapter/CategoriasAdapter.kt
@@ -0,0 +1,46 @@
+package com.hermen.learnenglish.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.hermen.learnenglish.R
+
+class CategoriasAdapter : RecyclerView.Adapter<CategoriasAdapter.ViewHolder> () {
+
+    var listCategory: MutableList<String> = ArrayList()
+    lateinit var context: Context
+
+    fun CategoriasAdapter (listCategorys : MutableList<String>, context: Context) {
+        this.context = context
+        this.listCategory = listCategorys
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CategoriasAdapter.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.item_menu, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: CategoriasAdapter.ViewHolder, position: Int) {
+        val item = listCategory.get(position)
+        holder.bind(item, context)
+    }
+
+    override fun getItemCount(): Int {
+        return listCategory.size
+    }
+
+    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+        val text = view.findViewById(R.id.textCategory) as TextView
+
+        fun  bind(category: String, context: Context) {
+            text.text = category
+        }
+    }
+}
+
--- app/src/main/java/com/hermen/learnenglish/vo/activity/MainActivity.kt
+++ app/src/main/java/com/hermen/learnenglish/vo/activity/MainActivity.kt
@@ -2,11 +2,38 @@ package com.hermen.learnenglish.vo.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hermen.learnenglish.R
+import com.hermen.learnenglish.adapter.CategoriasAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mReciclerView : RecyclerView
+    val mAdapter : CategoriasAdapter =  CategoriasAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var listCategory : MutableList<String> = ArrayList()
+
+        listCategory.add(0, "Category1")
+        listCategory.add(1, "Category2")
+        listCategory.add(2, "Category3")
+        listCategory.add(3, "Category4")
+        listCategory.add(4, "Category5")
+        listCategory.add(5, "Category6")
+        listCategory.add(6, "Category7")
+        listCategory.add(7, "Category8")
+        listCategory.add(8, "Category9")
+        listCategory.add(9, "Category10")
+        listCategory.add(10, "Category11")
+
+        mReciclerView = findViewById(R.id.rvCategory) as RecyclerView
+        mReciclerView.setHasFixedSize(true)
+        mReciclerView.layoutManager = LinearLayoutManager(this)
+        mAdapter.CategoriasAdapter(listCategory, this)
+        mReciclerView.adapter = mAdapter
     }
 }
